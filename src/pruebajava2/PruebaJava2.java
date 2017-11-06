d35c39e8809bbc320140dedaa58973fb11e52f17
alexolivaresdv <hp.alex.ao@gmail.com>
2017-11-06 18:18:42 -0300

    Creando Prueva java 2. Sincronizandolo y empujandolo

---
+++ src/pruebajava2/PruebaJava2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebajava2;
+
+/**************************************************************************
+ * Alex Olivares Del Valle 
+ * @author LAB17
+ * Fecha: Noviembre 06 del 2017
+ * Programa: Prueva Java 2
+ * Responsabilidad: utilizando estructura condiciones y bucles 
+ */
+public class PruebaJava2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
